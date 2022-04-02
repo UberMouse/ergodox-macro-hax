40fe98e3706108f6298e64ac73f5cf6682d848f8
Taylor Lodge <ubermouse894@gmail.com>
2022-04-03 00:32:10 +1300

    caroming macro

--- my-macros.ts
+++ my-macros.ts
@@ -10,6 +10,12 @@ const macroExtensions = {
     "dw": newMacro()
         .withShift(newMacro().typeAlphanumeric("1"))
         .withShift(newMacro().typeAlphanumeric("2")),
+    "carom": newMacro()
+        .withShift(newMacro().typeAlphanumeric("5"))
+        .delay(100)
+        .typeAlphanumeric("r")
+        .delay(300)
+        .withShift(newMacro().typeAlphanumeric("2"))
 }
 
 processAll(macroExtensions, SOURCE_DIR)
