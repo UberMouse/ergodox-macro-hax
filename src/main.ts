55510a1fd83701cf6464ff46c44bf98926f4ba41
Taylor Lodge <ubermouse894@gmail.com>
2025-10-11 23:36:47 +1300

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -21,7 +21,7 @@ export default async function main(
     const loaded = readFileSync(keymapSource).toString();
     const loadedWithv25Fix = loaded
         .replaceAll(")SS_DELAY", ") SS_DELAY")
-        .replaceAll(/\)\s\s/g, ") ");
+        .replaceAll(/\)  /g, ") ");
     console.log(loadedWithv25Fix)
     const newConfig = expandMacros(loadedWithv25Fix, macroMap);
 
